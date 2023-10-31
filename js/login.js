5ed73aad44a2cf1c22dc5a51478ed7337bfde213
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-10-31 14:46:36 +0200

    spa functions

--- js/login.js
+++ js/login.js
@@ -1,3 +1,29 @@
 function login(){
     
 }
+
+let currPage;
+document.addEventListener('DOMContentLoaded', () => {
+    changePage('log-in-template');
+    listenToActions();
+});
+function listenToActions() {
+    if (currPage === 'log-in-template') {
+        document.getElementById("log-in-btn").addEventListener('click', () => {
+            changePage('shoping-list-template');
+        });
+    } else if (currPage === 'shoping-list-template') {
+        // document.getElementById("sign-up-btn").addEventListener('click', () => {
+        //     changePage('sign-in-template');
+        // });
+    }
+}
+
+function changePage(newPageTemplateId) {
+    document.getElementById('div-hold').innerHTML = '';
+    let content = document.getElementById(newPageTemplateId).content;
+    let contentCopy = content.cloneNode(true);
+    document.getElementById('div-hold').appendChild(contentCopy);
+    currPage = newPageTemplateId;
+    listenToActions();
+}
