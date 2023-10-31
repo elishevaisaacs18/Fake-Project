73c927fdc30f5b3c8e2e9b8e264a22aa118d7711
Shirshlomo18 <Shirshlomo@hilma.tech>
2023-10-31 14:29:15 +0200

    second one

--- js/login.js
+++ js/login.js
@@ -1,3 +1,17 @@
-function login(){
-    
+const Users=[];
+function login(event){
+event.preventDefault();
 }
+let username=document.getElementById("user-name");
+let password=document.getElementById("password");
+ class User{
+    constractor(username,password){
+    this.username=username;
+    this.password=password;
+    this.id=User.length;
+    this.connected=false;
+    
+    }
+}
+ let json=JSON.stringify(user);
+ localStorage.setItem(json);
