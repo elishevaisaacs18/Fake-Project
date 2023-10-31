fb4c7dbf9366101b856b371a739bba9b62aa41de
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-10-31 16:37:50 +0200

    dbfunctions

--- js/login.js
+++ js/login.js
@@ -1,4 +1,6 @@
 const Users = [];
+const ShoppingItems = [];
+
 function login(event) {
     event.preventDefault();
 }
@@ -8,14 +10,21 @@ class User {
     constractor(username, password) {
         this.username = username;
         this.password = password;
-        this.id = User.length;
+        this.id = DB.getArrayOf("Users").length;
         this.connected = false;
-
     }
 }
 
-let json = JSON.stringify(user);
-localStorage.setItem(json);
+class ShoppingItem {
+    constractor(name) {
+        this.name = name;
+        this.id = DB.getArrayOf("Shopping Items").length;
+        this.deleted = false;
+    }
+}
+
+// let json = JSON.stringify(user);
+// localStorage.setItem(json);
 
 let currPage;
 document.addEventListener('DOMContentLoaded', () => {
@@ -41,4 +50,42 @@ function changePage(newPageTemplateId) {
     document.getElementById('div-hold').appendChild(contentCopy);
     currPage = newPageTemplateId;
     listenToActions();
+}
+
+class DB {
+    constractor() {
+        this.users = [];
+        this.shoppingItems = [];
+    }
+
+    getArrayOf(objName) {
+        return JSON.parse(localStorage.getItem(objName));
+    }
+
+    getObjById(id, objType) {
+        JSON.parse(this.getArrayOf(objType))[id];
+    }
+
+    deleteObj(objType, id) {
+        const ARR = getArrayOf(objType);
+        ARR[id] = {};
+        localStorage.setItem(objType, JSON.stringify(ARR));
+    }
+
+    addUser(username, password) {
+        let user = new User(username, password);
+        this.users.push(user);
+        localStorage.setItem("Users", JSON.stringify(this.users));
+
+    }
+    addShoppingItem(name) {
+        let item = new ShoppingItem(name);
+        this.shoppingItems.push(item);
+        localStorage.setItem("Shopping Items", JSON.stringify(this.shoppingItems));
+    }
+
+    getFilterdArrayByAttribure(arr, attribute, value) {
+        arr.filter((element) => element[attribute] === value);
+    }
+
 }
