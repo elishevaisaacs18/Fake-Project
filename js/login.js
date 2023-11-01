f2bba23bf693be55ea97eefd3c65c33afafb3cc9
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-01 11:56:55 +0200

    server function process request

--- js/login.js
+++ js/login.js
@@ -7,21 +7,21 @@ function login(event) {
 let username = document.getElementById("user-name");
 let password = document.getElementById("password");
 class User {
-    constractor(username, password) {
+    constructor(username, password) {
         this.username = username;
         this.password = password;
-        this.id = DB.getArrayOf("Users").length;
+        this.id = db.getArrayOf("Users").length;
         this.connected = false;
     }
 }
 
 class ShoppingItem {
-    constractor(name) {
+    constructor(name) {
         this.name = name;
-        this.id = DB.getArrayOf("Shopping Items").length;
+        this.id = db.getArrayOf("ShoppingItems").length;
         this.deleted = false;
     }
-} 
+}
 // function validPassword(password){
 //  let passwordCheck=/^(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 //     if(password.test(passwordCheck)){
@@ -64,21 +64,22 @@ function changePage(newPageTemplateId) {
 }
 
 class DB {
-    constractor() {
+    constructor() {
         this.users = [];
         this.shoppingItems = [];
     }
+
 
     getArrayOf(objName) {
         return JSON.parse(localStorage.getItem(objName));
     }
 
-    getObjById(id, objType) {
-        JSON.parse(this.getArrayOf(objType))[id];
+    getObjById(objType, id) {
+        return this.getArrayOf(objType)[id];
     }
 
     deleteObj(objType, id) {
-        const ARR = getArrayOf(objType);
+        const ARR = this.getArrayOf(objType);
         ARR[id] = {};
         localStorage.setItem(objType, JSON.stringify(ARR));
     }
@@ -86,26 +87,74 @@ class DB {
     addUser(username, password) {
         let user = new User(username, password);
         this.users.push(user);
+        console.log('usersssssss', this.users)
         localStorage.setItem("Users", JSON.stringify(this.users));
     }
 
     addShoppingItem(name) {
         let item = new ShoppingItem(name);
         this.shoppingItems.push(item);
-        localStorage.setItem("Shopping Items", JSON.stringify(this.shoppingItems));
+        localStorage.setItem("ShoppingItems", JSON.stringify(this.shoppingItems));
     }
 
     getFilterdArrayByAttribure(arr, attribute, value) {
-        arr.filter((element) => element[attribute] === value);
+        this.getArrayOf(arr).filter((element) => element[attribute] === value);
     }
 }
 
-class Server{
+localStorage.setItem("ShoppingItems", JSON.stringify(["milk", "cheese"]));
+localStorage.setItem("Users", JSON.stringify(["eli", "shir"]));
+const db = new DB();
+
+
+class Server {
     constructor() {
         this.state = 0;
     }
 
-    sendRequestToDb(request) {
+    sendRequestToDb(requestStr) {
+        const requestArr = requestStr.split(/\s|\/|\b/).filter(Boolean);
+        //[requestStr.split(" ").length - 1]
+        console.log('requestArr[1]: ', requestArr[1])
+        console.log('requestArr[2]: ', requestArr[2])
+        switch (true) {
+            case /^GET\s[A-Za-z]+$/.test(requestStr):
+                console.log("regex for get users");
+                console.log(db.getArrayOf(requestArr[1]));
+                //place 1 in the requestStr is the array name
+                break;
+            case /^GET\s[A-Za-z]+\/\d+$/.test(requestStr):
+                console.log("regex for get users/1, get shoppingItem/1");
+                console.log(db.getObjById(requestArr[1], requestArr[2]));
+                //place 1 in the requestStr is the array name, place 2 is the specific item id
+                break;
+            case /^DELETE\s[A-Za-z]+\/\d+$/.test(requestStr):
+                console.log("regex for DELETE users/0, DELETE shoppingItem/1");
+                console.log(db.deleteObj(requestArr[1], requestArr[2]));
+                //place 1 in the requestStr is the array name, place 2 is the specific item id
+                break;
+            case /^GET (\w+\/){2}\w+$/.test(requestStr):
+                console.log("regex for get users/userName/elisheva");
+                console.log(db.getFilterdArrayByAttribure(requestArr[1], requestArr[2], requestArr[3]))
+                //place 1 in the requestStr is the array name, place 2 is the attribute, place 3 is the attribute value
+                break;
+            case /^POST(\s[A-Za-z]+){3}$/.test(requestStr):
+                console.log("regex for POST users username elisheva ");
+                console.log(db.addUser(requestArr[2], requestArr[3]))
+                //place 2 is the username, place 3 is the password
+                break;
+            case /^POST(\s[A-Za-z]+){2}$/.test(requestStr):
+                console.log("regex for POST ShoppingItem oil");
+                console.log(db.addShoppingItem(requestArr[2]))
+                //place 2 in the requestStr is the name 
+                break;
+            default:
+                console.log("Request not recognized.");
+        }
+
+        // Example usage:
+
+
         // regex for get users       /^GET\s\/[A-Za-z]/$/
         // regex for get users/1, get shoppingItem/1      /^GET\s\/[A-Za-z]/\/\d+$/
         // regex for DELETE users/0, DELETE shoppingItem/1      /^DELETE\s\/[A-Za-z]/\/\d+$/
@@ -116,4 +165,25 @@ class Server{
     //validation unams pass
     //url to a db function
     //return value to client
+}
+
+let server = new Server();
+server.sendRequestToDb("GET Users");
+server.sendRequestToDb("GET Users/1");
+server.sendRequestToDb("DELETE Users/1");
+server.sendRequestToDb("GET Users/name/elisheva");
+server.sendRequestToDb("POST Users username elisheva");
+server.sendRequestToDb("POST ShoppingItems oil");
+server.sendRequestToDb("POST Users eliaaa aaaaaa");
+server.sendRequestToDb("POST ShoppingItems aaa");
+
+
+
+
+function sendFAJAXToServer(requestStr) {
+    setTimeout(Server.sendRequestToDb(requestStr), 30000);
+}
+
+function sendServerDataToClient(dava) {
+    //setTimeout(Server.translateRequest(requestStr), 30000);
 }
