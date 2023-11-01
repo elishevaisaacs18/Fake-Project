0ca6d253a7098c4123b92cff25a997e760f8e24e
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-01 18:38:29 +0200

    shir tries

--- js/login.js
+++ js/login.js
@@ -1,17 +1,69 @@
 localStorage.setItem("Users", JSON.stringify([{ name: "eli", password: "eli", id: 0, connected: true }]));
-
+// function logOut (){
+//     let logOutn=document.getElementById("log-out");
+//     if(logOutn){
+//         localStorage.removeItem(getCurrUser);
+//     }
+// }
 function login(event) {
     event.preventDefault();
 }
 let username = document.getElementById("user-name");
 let password = document.getElementById("password");
+
+class DB {
+    constructor() {
+        this.users = [];
+    }
+
+    getArrayOf(objName) {
+        return JSON.parse(localStorage.getItem(objName));
+    }
+
+    getObjById(objType, id) {
+        return this.getArrayOf(objType)[id];
+    }
+
+    deleteObj(objType, id) {
+        const ARR = this.getArrayOf(objType);
+        ARR[id] = {};
+        localStorage.setItem(objType, JSON.stringify(ARR));
+        return "success";
+    }
+
+    addUser(username, password) {
+        let user = new User(username, password);
+        this.users.push(user);
+        localStorage.setItem("Users", JSON.stringify(this.users));
+        return "success";
+    }
+
+    addShoppingItem(name) {
+        let item = new ShoppingItem(name);
+        const user = getCurrUser();
+        const shoppingArr = Array.from(user.shoppingList);
+        shoppingArr.push(item);
+        user.shoppingList = shoppingArr;
+        this.users[getCurrUser().id] = user;
+        localStorage.setItem("Users", JSON.stringify(this.users));
+        console.log('getCurrUser().shoppingList): ', getCurrUser().shoppingList)
+        return "success";
+    }
+
+    getFilterdArrayByAttribure(arr, attribute, value) {
+        return this.getArrayOf(arr).filter((element) => element[attribute] === value);
+    }
+}
+
+const db = new DB();
+
 class User {
     constructor(username, password) {
         this.username = username;
         this.password = password;
         this.shoppingList = new ShoppingList();
         this.id = db.getArrayOf("Users").length - 1;
-        this.connected = true;
+        this.connected = true;/////////////////////////////////////////////////////////////
     }
 }
 
@@ -22,11 +74,90 @@ class ShoppingList {
     }
 }
 
-
 class ShoppingItem {
     constructor(name) {
         this.name = name;
         this.deleted = false;
+    }
+}
+
+class Server {
+    constructor() {
+        this.state = 0;
+    }
+
+    sendRequestToDb(requestStr) {
+        const requestArr = requestStr.split(/\s|\/|\b/).filter(Boolean);
+        switch (true) {
+            case /^GET\s[A-Za-z]+$/.test(requestStr):
+                sendServerDataToClient(db.getArrayOf(requestArr[1]));
+                //place 1 in the requestStr is the array name
+                break;
+            case /^GET\s[A-Za-z]+\/\d+$/.test(requestStr):
+                sendServerDataToClient(db.getObjById(requestArr[1], requestArr[2]));
+                //place 1 in the requestStr is the array name, place 2 is the specific item id
+                break;
+            case /^DELETE\s[A-Za-z]+\/\d+$/.test(requestStr):
+                sendServerDataToClient(db.deleteObj(requestArr[1], requestArr[2]));
+                //place 1 in the requestStr is the array name, place 2 is the specific item id
+                break;
+
+
+
+                //delete item 
+            // case /^DELETE\s[A-Za-z]+\/\d+$/.test(requestStr):
+            //     sendServerDataToClient(db.deleteObj(requestArr[1], requestArr[2]));
+            //     //place 1 in the requestStr is the array name, place 2 is the specific item id
+            //     break;
+
+
+
+            case /^GET (\w+\/){2}\w+$/.test(requestStr):
+                sendServerDataToClient(db.getFilterdArrayByAttribure(requestArr[1], requestArr[2], requestArr[3]))
+                //place 1 in the requestStr is the array name, place 2 is the attribute, place 3 is the attribute value
+                break;
+            case /^POST(\s[A-Za-z]+){3}$/.test(requestStr):
+                sendServerDataToClient(db.addUser(requestArr[2], requestArr[3]))
+                //place 2 is the username, place 3 is the password
+                break;
+            case /^POST(\s[A-Za-z]+){2}$/.test(requestStr):
+                sendServerDataToClient(db.addShoppingItem(requestArr[2]))
+                //place 2 in the requestStr is the name 
+                break;
+            default:
+                console.log("Request not recognized.");
+        }
+    }
+    //validation unams pass
+    //return value to client
+}
+
+
+let server = new Server();
+server.sendRequestToDb("POST Users username elisheva");
+server.sendRequestToDb("POST ShoppingItems oil");
+server.sendRequestToDb("POST Users eliaaa aaaaaa");
+server.sendRequestToDb("POST ShoppingItems aaa");
+server.sendRequestToDb("GET Users");
+server.sendRequestToDb("GET Users/0");
+server.sendRequestToDb("DELETE Users/1");
+server.sendRequestToDb("GET Users/username/username");
+
+class FakeAjax {
+    constructor() {
+        this.responseText = '';
+
+    }
+    open(type, route, body) {
+        console.log("open")
+        this.responseText = `${type} ${route}${body ? " " + body : ""}`;
+        //type + " " + route + body? +" " + body : "";
+        console.log('this.responseText: ', this.responseText);
+    }
+    send() {
+        server.sendRequestToDb(this.responseText);
+        console.log('this.responseText: ', this.responseText)
+        console.log('sent');
     }
 }
 
@@ -35,16 +166,20 @@ function addItemPrompt() {
     if (item !== "") {
         let listItem = document.createElement("li");
         listItem.textContent = item;
-        // listItem.id=listItem;
-        let parentElement = document.getElementById("ul-list");
-        parentElement.appendChild(listItem);
+        document.getElementById("ul-list").appendChild(listItem);
         let icon = document.createElement("img");
         icon.src = "../img/remove-item.png";
-        icon.id = "remove - item";
-        parentElement.appendChild(icon);
-        icon.addEventListener('click', console.log("faja"));
+         icon.class= "remove - item";
+        listItem.appendChild(icon);
+        icon.addEventListener("click", () => sendFAJAXToServer(item))
     }
-
+}
+function sendFAJAXToServer(item) {
+    console.log("deleted");
+    console.log('route', "DELETE", "Users/" + getCurrUser().id + "/" + item)
+    const fajax = new FakeAjax();
+    fajax.open("DELETE", "Users/" + getCurrUser().id + "/" + item);
+    fajax.send();
 }
 // function validPassword(password){
 //  let passwordCheck=/^(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
@@ -89,127 +224,9 @@ function changePage(newPageTemplateId) {
     listenToActions();
 }
 
-class DB {
-    constructor() {
-        this.users = [];
-    }
-
-
-    getArrayOf(objName) {
-        return JSON.parse(localStorage.getItem(objName));
-    }
-
-    getObjById(objType, id) {
-        return this.getArrayOf(objType)[id];
-    }
-
-    deleteObj(objType, id) {
-        const ARR = this.getArrayOf(objType);
-        ARR[id] = {};
-        localStorage.setItem(objType, JSON.stringify(ARR));
-        return "success";
-    }
-
-    addUser(username, password) {
-        let user = new User(username, password);
-        this.users.push(user);
-        localStorage.setItem("Users", JSON.stringify(this.users));
-        return "success";
-    }
-
-    addShoppingItem(name) {
-        let item = new ShoppingItem(name);
-        const user = getCurrUser();
-        const shoppingArr = Array.from(user.shoppingList);
-        shoppingArr.push(item);
-        user.shoppingList = shoppingArr;
-        this.users[getCurrUser().id] = user;
-        localStorage.setItem("Users", JSON.stringify(this.users));
-        console.log('getCurrUser().shoppingList): ', getCurrUser().shoppingList)
-        return "success";
-    }
-
-    getFilterdArrayByAttribure(arr, attribute, value) {
-        return this.getArrayOf(arr).filter((element) => element[attribute] === value);
-    }
-}
-
-const db = new DB();
-
-class Server {
-    constructor() {
-        this.state = 0;
-    }
-
-    sendRequestToDb(requestStr) {
-        const requestArr = requestStr.split(/\s|\/|\b/).filter(Boolean);
-        switch (true) {
-            case /^GET\s[A-Za-z]+$/.test(requestStr):
-                sendServerDataToClient(db.getArrayOf(requestArr[1]));
-                //place 1 in the requestStr is the array name
-                break;
-            case /^GET\s[A-Za-z]+\/\d+$/.test(requestStr):
-                sendServerDataToClient(db.getObjById(requestArr[1], requestArr[2]));
-                //place 1 in the requestStr is the array name, place 2 is the specific item id
-                break;
-            case /^DELETE\s[A-Za-z]+\/\d+$/.test(requestStr):
-                sendServerDataToClient(db.deleteObj(requestArr[1], requestArr[2]));
-                //place 1 in the requestStr is the array name, place 2 is the specific item id
-                break;
-            case /^GET (\w+\/){2}\w+$/.test(requestStr):
-                sendServerDataToClient(db.getFilterdArrayByAttribure(requestArr[1], requestArr[2], requestArr[3]))
-                //place 1 in the requestStr is the array name, place 2 is the attribute, place 3 is the attribute value
-                break;
-            case /^POST(\s[A-Za-z]+){3}$/.test(requestStr):
-                sendServerDataToClient(db.addUser(requestArr[2], requestArr[3]))
-                //place 2 is the username, place 3 is the password
-                break;
-            case /^POST(\s[A-Za-z]+){2}$/.test(requestStr):
-                sendServerDataToClient(db.addShoppingItem(requestArr[2]))
-                //place 2 in the requestStr is the name 
-                break;
-            default:
-                console.log("Request not recognized.");
-        }
-    }
-    //validation unams pass
-    //return value to client
-}
-
-
-let server = new Server();
-server.sendRequestToDb("POST Users username elisheva");
-server.sendRequestToDb("POST ShoppingItems oil");
-server.sendRequestToDb("POST Users eliaaa aaaaaa");
-server.sendRequestToDb("POST ShoppingItems aaa");
-server.sendRequestToDb("GET Users");
-server.sendRequestToDb("GET Users/0");
-server.sendRequestToDb("DELETE Users/1");
-server.sendRequestToDb("GET Users/username/username");
-
-class FakeAjax {
-    constructor() {
-        this.responseText = '';
-
-    }
-    open(type, route, body) {
-        this.responseText = `${type} ${route}${body ? " " + body : ""}`;
-        //type + " " + route + body? +" " + body : "";
-        console.log('this.responseText: ', this.responseText);
-    }
-    send (){
-        server.sendRequestToDb(this.responseText) ;
-        console.log('sent');
-    }
-}
-
-const fajax = new FakeAjax() 
-fajax.open("POST", "Users", "mmmm");
-fajax.send();
-
 //Network
 function sendFAJAXToServer(requestStr) {
-    setTimeout(Server.sendRequestToDb(requestStr), 30000);
+    setTimeout(server.sendRequestToDb(requestStr), 30000);
 }
 
 function sendServerDataToClient(data) {
